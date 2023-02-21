574b21fe054e09320427c4558d8d2d57670b9297
Josea5abio <joseantsabiomolina@gmail.com>
2023-02-21 09:21:47 +0100

    arreglo otra frase huevo

--- huevo.java
+++ huevo.java
@@ -2,5 +2,6 @@ public class huevo{
     int peso;
     public void decirpeso(){
         System.out.println("El huevo pesa"+this.peso+"g");
+        System.out.println("El huevo esta en definicion");
     }
 }
